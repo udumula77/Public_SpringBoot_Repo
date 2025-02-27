99a32e3b9aabda32246762e1fff11cd2ebc49565
udumula77 <udumula.anilkumar77@gmail.com>
2025-02-27 16:14:12 -0500

    first java commit

---
+++ corejava/src/main/java/corejava/JavaMaven.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public class JavaMaven{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ corejava/src/test/java/corejava/JavaMavenTest.java
@@ -0,0 +1,16 @@
+package corejava;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class JavaMavenTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
